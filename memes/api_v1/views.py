1b6b84b9a412cd5df4093d7afb9a6c353a0ff494
Ilya Bagrov <ilya.b.only@gmail.com>
2024-07-17 23:50:32 +0300

    Add all CRUD endpoints

--- memes/api_v1/views.py
+++ memes/api_v1/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy import Result, select
 from sqlalchemy.orm import Session
 
@@ -27,3 +27,35 @@ def get_memes(offset: int = 0, limit: int = 5, db: Session = Depends(get_db_sess
     memes = result.scalars().all()
     return memes
 
+
+@router.get('/memes/{id}', response_model=schemas.Meme)
+def get_meme(id: int, db: Session = Depends(get_db_session)):
+    meme = db.get(Meme, id) 
+    return meme
+
+
+@router.post('/memes', response_model=schemas.Meme, status_code=status.HTTP_201_CREATED)
+def create_meme(new_meme: schemas.MemeCreate, db: Session = Depends(get_db_session)):
+    meme = Meme(**new_meme.model_dump())
+    db.add(meme)
+    db.commit()
+    db.refresh(meme)
+    return meme
+
+
+@router.put('/memes/{id}', response_model=schemas.Meme)
+def update_meme(id: int, new_data: schemas.MemeUpdate, db: Session = Depends(get_db_session)):
+    meme = db.get(Meme, id)
+    for key, val in new_data.model_dump(exclude_none=True).items():
+        meme.__setattr__(key, val)
+    db.flush([meme])
+    db.commit()
+    db.refresh(meme)
+    return meme
+
+
+@router.delete('/memes/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_meme(id:int, db: Session = Depends(get_db_session)):
+    meme = db.get(Meme, id)
+    db.delete(meme)
+    db.commit()
