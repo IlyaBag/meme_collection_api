8bd4d4a36b5f94f6c9444393a9744c8cf183082a
Ilya Bagrov <ilya.b.only@gmail.com>
2024-07-17 23:49:41 +0300

    Add schema 'MemeUpdate'

--- memes/schemas.py
+++ memes/schemas.py
@@ -2,8 +2,20 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class Meme(BaseModel):
-    id: int
+class MemeBase(BaseModel):
     text: str
     img_url: str
+
+
+class MemeCreate(MemeBase):
+    pass
+
+
+class MemeUpdate(MemeBase):
+    text: str | None = None
+    img_url: str | None = None
+
+
+class Meme(MemeBase):
+    id: int
     created_at: datetime
