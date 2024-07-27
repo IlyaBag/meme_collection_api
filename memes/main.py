51fffe20f1043d26af33651288b88d04fabee371
Ilya Bagrov <ilya.b.only@gmail.com>
2024-07-28 02:43:40 +0300

    Add database creation when application starts

--- memes/main.py
+++ memes/main.py
@@ -1,8 +1,18 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from api_v1.views import router
+from db import engine
+from models import Base
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router)
