532e1cc8b830d880376327d367b0bdf7cd56f16e
Ilya Bagrov <ilya.b.only@gmail.com>
2024-07-28 01:46:47 +0300

    Update constants in config.py

--- memes/config.py
+++ memes/config.py
@@ -5,8 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-POSTGRES_USER = os.getenv('POSTGRES_USER')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+DB_ECHO = os.getenv('DB_ECHO', '').lower() in ('true', 'yes', 'y', '1')
+
+DB_USER = os.getenv('POSTGRES_USER')
+DB_PASS = os.getenv('POSTGRES_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
-DB_NAME = POSTGRES_USER
+DB_NAME = DB_USER
