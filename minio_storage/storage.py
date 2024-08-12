ef49088a4f733505823c2edcd100e9b53428678b
Ilya Bagrov <ilya.b.only@gmail.com>
2024-08-12 08:06:51 +0300

    Implement minio storage and its API

--- minio_storage/storage.py
+++ minio_storage/storage.py
@@ -1,5 +1,8 @@
+# import io
+# import sys
+from datetime import timedelta
 from typing import BinaryIO
-from minio import Minio
+from minio import Minio  #, S3Error
 
 from config import MINIO_ACCESS_KEY, MINIO_BUCKET_NAME, MINIO_SECRET_KEY
 
@@ -15,18 +18,65 @@ if not found:
     client.make_bucket(MINIO_BUCKET_NAME)
 
 
-def save_obj(object_name: str, object_body: BinaryIO):
+def save_obj(object_name: str, object_body: BinaryIO) -> str:
     '''Save new object in the storage.'''
-    ...
+    obj = client.put_object(
+        bucket_name=MINIO_BUCKET_NAME,
+        object_name=object_name,
+        data=object_body,
+        length=-1,
+        part_size=5*1024*1024,
+    )
+    return obj.object_name
 
-def read_obj(id):
-    '''Get object from the storage by ID.'''
-    ...
+def read_obj(object_name: str) -> bytes:
+    '''Get object from the storage.'''
+    try:
+        response = client.get_object(MINIO_BUCKET_NAME, object_name)
+        # Read data from response.
+        return response.data
+    finally:
+        response.close()
+        response.release_conn()
 
-def rewrite_obj(id):
-    '''Update (replace) object in the storage.'''
-    ...
+def get_obj_link(object_name: str) -> str:
+    '''Get link to download the object.'''
+    link = client.get_presigned_url('GET', MINIO_BUCKET_NAME, object_name,
+                                    expires=timedelta(minutes=5))
+    return link
 
-def remove_obj(id):
+def remove_obj(object_name: str) -> None:
     '''Delete object from the storage.'''
-    ...
+    client.remove_object(MINIO_BUCKET_NAME, object_name)
+
+# def main():
+    # client = Minio("play.min.io",
+    #     access_key="Q3AM3UQ867SPQQA43P2F",
+    #     secret_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+    # )
+
+    # source_file = '/home/ilba/my_code/for_mad_soft/^_^tasks.md'
+    # bucket_name = 'first'
+    # destination_file = 'tasks.md'
+
+    # found = client.bucket_exists(bucket_name)
+    # if not found:
+    #     client.make_bucket(bucket_name)
+    #     print('Created bucket', bucket_name)
+    # else:
+    #     print('Bucket', bucket_name, 'already exists')
+
+    # client.fput_object(bucket_name, destination_file, source_file)
+    # print(
+    #     source_file, 'successfully uploaded as object', destination_file,
+    #     'to bucket', bucket_name
+    # )
+#     with open('requirements.txt', 'rb') as obj:
+#         result = client.put_object(bucket_name, 'bytes_txt.txt', obj, -1, part_size=5*1024*1024)
+#         f'Object {result.object_name} created.'
+
+# if __name__ == '__main__':
+#     try:
+#         main()
+#     except S3Error as exc:
+#         print('error occurred.', exc)
--- minio_storage/views.py
+++ minio_storage/views.py
@@ -1,25 +1,35 @@
-from fastapi import APIRouter
+import uuid
 
+from fastapi import APIRouter, UploadFile
+
+import storage
 
 
 router = APIRouter(prefix='/api/v1', tags=['MinIO Storage'])
 
 
 @router.post('/objects')
-def create_obj():
-    ...
+def create_obj(img: UploadFile):
+    img_name_prefix = str(uuid.uuid4())
+    img_name = f'{img_name_prefix}_{img.filename}'
+    saved_img_name = storage.save_obj(img_name, img.file)
+    return saved_img_name if saved_img_name else None    # TODO: raise exception
 
 
-@router.get('/objects/{id}')
-def get_obj(id):
-    ...
+@router.get('/objects/{name}')
+def get_obj(name: str):
+    # object = storage.read_obj(name)
+    # return str(object)
+    return storage.get_obj_link(name)
 
 
-@router.put('objects/{id}')
-def update_obj(id):
-    ...
+@router.put('objects/{name}')
+def update_obj(name: str, img: UploadFile):
+    storage.save_obj(name, img.file)
+    return {'status': 'success', 'details': 'Image saved'}
 
 
-@router.delete('objects/{id}')
-def delete_obj(id):
-    ...
+@router.delete('objects/{name}')
+def delete_obj(name: str):
+    storage.remove_obj(name)
+    return {'status': 'success', 'details': 'Image deleted'}
